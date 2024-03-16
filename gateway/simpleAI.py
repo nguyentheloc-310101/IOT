83b1ae4021224b06f942f436e81b3a26bb7d3f7c
k-d-7 <duy.nguyen60201@hcmut.edu.vn>
2024-03-16 16:52:35 +0700

    change model

--- gateway/simpleAI.py
+++ gateway/simpleAI.py
@@ -6,13 +6,13 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model("keras_Model.h5", compile=False)
+model = load_model("keras_model.h5", compile=False)
 
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
 
 # CAMERA can be 0 or 1 based on default camera of your computer
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(0)
 
 while True:
     # Grab the webcamera's image.
