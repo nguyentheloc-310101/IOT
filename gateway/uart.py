9a9425ff323de7cb5e800da693568c5dda4489ad
loc-dev <nguyentheloc310101@gmail.com>
2024-04-25 15:49:34 +0700

    update:new

--- gateway/uart.py
+++ gateway/uart.py
@@ -1,18 +1,18 @@
-import serial.tools.list_ports
+import serial.tools.list_ports # type: ignore
 
 
 def getPort():
-    ports = serial.tools.list_ports.comports()
-    N = len(ports)
-    commPort = "None"
-    for i in range(0, N):
-        port = ports[i]
-        strPort = str(port)
-        if "USB Serial Device" in strPort:
-            splitPort = strPort.split(" ")
-            commPort = splitPort[0]
+    # ports = serial.tools.list_ports.comports()
+    # N = len(ports)
+    # commPort = "None"
+    # for i in range(0, N):
+    #     port = ports[i]
+    #     strPort = str(port)
+    #     if "USB Serial Device" in strPort:
+    #         splitPort = strPort.split(" ")
+    #         commPort = splitPort[0]
     # return commPort
-    return "COM8"
+    return "COM1"
 
 
 if getPort() != "None":
@@ -21,10 +21,11 @@ if getPort() != "None":
 
 
 def processData(client, data):
+    print("data wite:",data)
+    
     data = data.replace("!", "")
     data = data.replace("#", "")
     splitData = data.split(":")
-    print("data wite:",data)
     if splitData[0] == "T":
         client.publishMessage("theloc3101/feeds/temperature_history", splitData[1])
     elif splitData[0] == "H":
@@ -36,45 +37,24 @@ def processData(client, data):
 mess = ""
 
 
-# def readSerial(client):
-#     bytesToRead = ser.inWaiting()
-#     if bytesToRead > 0:
-#         global mess
-#         mess = mess + ser.read(bytesToRead).decode("UTF-8")
-#         print(mess)
-#         while ("#" in mess) and ("!" in mess):
-#             start = mess.find("!")
-#             end = mess.find("#")
-#             processData(client, mess[start : end + 1])
-#             if end == len(mess):
-#                 mess = ""
-#             else:
-#                 mess = mess[end + 1 :]
 def readSerial(client):
     bytesToRead = ser.inWaiting()
+    # print("bytesToRead: ",bytesToRead)
+    
     if bytesToRead > 0:
         global mess
-        data = ser.read(bytesToRead)
-        if data:
-            try:
-                decoded_data = data.decode("ascii")
-                mess = mess + decoded_data
-                print("mess from serial read: ", mess)
-                while "#" in mess and "!" in mess:
-                    start = mess.find("!")
-                    end = mess.find("#")
-                    if start != -1 and end != -1:  # Check if both delimiters are found
-                        message = mess[start + 1:end]  # Extract the message between delimiters
-                        processData(client, message)
-                        mess = mess[end + 1:]  # Remove processed data from the buffer
-                    else:
-                        # Incomplete message, wait for more data
-                        break
-            except UnicodeDecodeError as e:
-                print(f"UnicodeDecodeError: {e}")
-
-                # Handle the error gracefully, such as logging it or trying different encodings
-
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        print("read: ",mess)
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(client, mess[start : end + 1])
+            if end == len(mess):
+                mess = ""
+            else:
+               mess = mess[end + 1 :]
+    else:
+        return
 
 
 
